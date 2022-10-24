21ccb833d3ffb9820cbe237d97a966581a958a7c
Javon Scott <116174058+greekcodes@users.noreply.github.com>
2022-10-24 12:31:06 +0000

    Finished tax_rate function, Added calculation function

--- main.py
+++ main.py
@@ -13,10 +13,18 @@ def set_tax_rate():
              
              
              }
+  tax_rate = tax_dict[state]
+  return tax_rate
 # We need price of an item
+
 
 def get_item():
   item_name = input("What item are you buying?")
   price = float(input("What is the price of your item?"))
   return item_name, price
+
+  def calculate_tax(price, tax_rate):
+    sales_tax = price * tax_rate 
+    total = price + sales_tax
+    return sales_tax, total
   
